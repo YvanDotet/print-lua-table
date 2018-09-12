6e9cca70ca5ecabe0da1470afc29be3655a16b9a
YvanDotet <40297112+YvanDotet@users.noreply.github.com>
2018-09-12 17:42:25 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,33 +1,45 @@
 function pprint( t ) 
-    local same={} // pour éviter des répétitions
+  local same={} -- éviter les répétitions
 
-    local function _print(t,indent)
-        if (same[tostring(t)]) then
-            print(indent.."*"..tostring(t))
-        else
-            same[tostring(t)]=true
-            if (type(t)=="table") then
-                for pos,val in pairs(t) do
-                    if (type(val)=="table") then
-                        print(indent.."["..tostring(pos).."]>"..tostring(val).." {")
-                        _print(val,indent..string.rep(" ",string.len(tostring(pos))+8))
-                        print(indent..string.rep(" ",string.len(tostring(pos))+6).."}")
-                    else
-                        print(indent.."["..tostring(pos).."]>"..tostring(val))
-                    end
-                end
-            else
-                print(indent..tostring(t))
-            end
-        end
+  local function _print(t,indent)
+    if type(t)~="table" then return tostring( t ) end
+    if same[tostring(t)] then return "*"..tostring(t) end
+
+    same[tostring(t)]=true
+    
+    local sg=tostring(t).." {".."\n"
+  
+    for pos,val in pairs(t) do
+      local len=string.len( indent.."["..tostring(pos).."]>".."  " )
+      local toAdd = string.rep( " ", len )
+      
+      sg=sg..indent.."["..tostring(pos).."]>".._print(val,toAdd).."\n"
     end
 
-    if (type(t)=="table") then
-        print(tostring(t).." {")
-        _print(t,"  ")
-        print("}")
-    else
-        _print(t,"  ")
-    end
-    print()
+    return sg..string.sub(indent,0,-3).."}"
+  end
+
+  print( _print(t,"  ") )
 end
+}
+
+--Exemple
+
+local t={1,{"abc",{}}}
+t[{}]=true
+t[function() end]=t
+
+pprint(t)
+
+--[[
+table: 0A9258F8 {
+  [1]>1
+  [2]>table: 0A9257B8 {
+        [1]>abc
+        [2]>table: 0A925A10 {
+            }
+      }
+  [table: 0A9255D8]>true
+  [function: 0AFD3370]>*table: 0A9258F8
+}
+]]--
